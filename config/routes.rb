a0347b4060b9b8c403a31889197e66606054c001
Michael Slone <m.slone@gmail.com>
2011-11-04 16:26:58 -0400

    start service controller

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ Identity::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "service#index"
 
   # See how all your routes lay out with "rake routes"
 
