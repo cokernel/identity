d50c7e2fa5d0078217a2c16b27c1b3f989b45cff
Michael Slone <m.slone@gmail.com>
2012-02-02 09:13:57 -0500

    use JSON output for Get-Service-State

--- app/controllers/service_controller.rb
+++ app/controllers/service_controller.rb
@@ -1,4 +1,10 @@
 class ServiceController < ApplicationController
+  include ApplicationHelper
+
   def index
+    render :json => { 
+      'Service instantiation' => uuid,
+      'Shoulder spaces' => [],
+    }
   end
 end
